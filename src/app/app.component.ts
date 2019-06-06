68e423a15689950a4ed83cdce7af0c38016fad69
chuma <chumadamane40@gmail.com>
2019-06-06 14:14:23 +0200

    ToDoList

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; 
+import { addListener } from 'cluster';
+import { ViewEncapsulation } from '@angular/compiler/src/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent {
   title = 'ToDoList';
+tasks = [];
+
+  newTask : string
+
+  addTasks(){
+this.tasks.push(this.newTask)
+this.newTask = null
+  }
+
+RemoveTask(task) {
+  let index = this.tasks.indexOf(task)
+  this.tasks.splice(index,1)
 }
+  }
+
